c0bb489b7f8f58726135537299bcac91a0240653
José Luis Samper <jluis.samper@gmail.com>
2015-07-12 22:37:23 +0200

    Adición de búsquedas a programa base del módulo 6

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -15,9 +15,19 @@ exports.load = function (req,res,next,quizId) {
 
 //GET quizes 
 exports.index = function (req,res) {
-    models.Quiz.findAll().then(function(quizes) {
-        res.render('quizes/index', {quizes: quizes});
-    })
+    // get the parameter and choose if the table has to be filtered or not
+    // we are looking for a word or a cluster of words so we have to deal with spaces, colons ... that could appear in the whole
+    // question. This can be done by %word1%word2%
+    var busqueda = req.query.search;
+    if(!busqueda) {
+        models.Quiz.findAll().then(function(quizes) {
+            res.render('quizes/index', {quizes: quizes});
+        })
+    } else {
+        models.Quiz.findAll({where: ["pregunta like ?", ("%" + busqueda + "%").replace(' ', '%')]}).then(function(quizes) {
+            res.render('quizes/index', {quizes: quizes});
+        })
+    }
 };
 // GET /quizes/show
 
